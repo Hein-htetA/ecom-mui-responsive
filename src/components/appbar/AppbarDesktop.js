cdc27250c523622ea6736814caf394859ae5d889
Hein Htet <heinha123456@gmail.com>
2022-08-02 21:15:06 +0630

    MyList in Appbar Container

--- src/components/appbar/AppbarDesktop.js
+++ src/components/appbar/AppbarDesktop.js
@@ -1,4 +1,5 @@
-import { AppbarContainer, AppbarHeader } from "../../styles/appbar";
+import { ListItemText } from "@mui/material";
+import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
 
 export default function AppbarDesktop({ matches }) {
     return (
@@ -9,6 +10,12 @@ export default function AppbarDesktop({ matches }) {
          */
         <AppbarContainer>
             <AppbarHeader>My Bags</AppbarHeader>
+            <MyList type='row'>
+                <ListItemText primary="Home"/>
+                <ListItemText primary="Categories"/>
+                <ListItemText primary="Products"/>
+                <ListItemText primary="Contact us"/>
+            </MyList>
         </AppbarContainer>
     )
 }
--- src/styles/appbar/index.js
+++ src/styles/appbar/index.js
@@ -1,7 +1,7 @@
 import { Box, Typography } from '@mui/material';
-import { Colors } from '../theme';
 import { styled } from '@mui/material/styles';
 import '@fontsource/montez';
+import List from '@mui/material/List';
 
 //container
 
@@ -11,13 +11,21 @@ export const AppbarContainer = styled(Box)(() => ({
     justifyContent: "center",
     alignItems: 'center',
     padding: '2px 8px'
-}))
+}));
 
 //header
-export const AppbarHeader = styled(Typography)(() => ({
-    color: Colors.secondary,
+export const AppbarHeader = styled(Typography)(({theme}) => ({
+    color: `${theme.palette.secondary.main}`,
     padding: '4px',
     fontSize: '4em',
     fontFamily: '"Montez", "cursive"',
-    flexGrow: 1,
-}))
+    flexGrow: 1
+
+}));
+
+export const MyList = styled(List)(({ type }) => ({
+    display: type === 'row' ? 'flex' : 'block',
+    flexGrow: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+}));
