604bff6a988e66b49ec057b6326b84aacdfb9173
Hein Htet <heinha123456@gmail.com>
2022-08-03 12:00:19 +0630

    App bar list fix width

--- src/components/appbar/AppbarDesktop.js
+++ src/components/appbar/AppbarDesktop.js
@@ -1,5 +1,6 @@
-import { ListItemText } from "@mui/material";
+import { IconButton, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function AppbarDesktop({ matches }) {
     return (
@@ -10,11 +11,18 @@ export default function AppbarDesktop({ matches }) {
          */
         <AppbarContainer>
             <AppbarHeader>My Bags</AppbarHeader>
-            <MyList type='row'>
+            <MyList type='row' sx={{'& > div': {
+                width: '10vw'
+            }}}>
                 <ListItemText primary="Home"/>
                 <ListItemText primary="Categories"/>
                 <ListItemText primary="Products"/>
                 <ListItemText primary="Contact us"/>
+                <ListItemButton>
+                    
+                        <SearchIcon />
+                  
+                </ListItemButton>
             </MyList>
         </AppbarContainer>
     )
--- src/styles/appbar/index.js
+++ src/styles/appbar/index.js
@@ -29,3 +29,4 @@ export const MyList = styled(List)(({ type }) => ({
     justifyContent: 'center',
     alignItems: 'center'
 }));
+
